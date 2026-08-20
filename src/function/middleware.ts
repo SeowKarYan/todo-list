9dad3e62243e88cc2a7561b437cccb37cb612b40
SeowKarYan <sky0228@1utar.my>
2026-08-20 22:43:33 +0800

    done list api

--- src/function/middleware.ts
+++ src/function/middleware.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import type { Request, Response, NextFunction } from "express";
 
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const sessionAuthentication = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.session;
 
     if (!token) {
--- src/routes/list.ts
+++ src/routes/list.ts
@@ -1,22 +1,90 @@
 import express, { type Request, type Response } from 'express';
-import { authenticate } from '../function/middleware.ts';
+import { sessionAuthentication } from '../function/middleware.ts';
+import List from '../model/list.ts';
 
 const router = express.Router();
 
-router.post("", authenticate, async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "This is get user API" })
+router.post("", sessionAuthentication, async (req: Request, res: Response) => {
+    try {
+        const { title, description } = req.body;
+        if (!title || !description) {
+            return res.status(400).json({ message: "Title and description are required", status: "failed" });
+        }
+
+        await List.create({ title, description, userId: req.user?.id });
+
+        return res.status(201).json({ message: "List created successfully", status: "success" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Error", status: "failed" })
+    }
 })
 
-router.get("/:id", authenticate, async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "This is post user API" })
+router.get("/", sessionAuthentication, async (req: Request, res: Response) => {
+    try {
+        const foundList = await List.findAll({ where: { userId: req.user?.id }, order: [['updatedAt', 'DESC']] });
+
+        return res.status(200).json({ message: "List retrieved successfully", data: foundList, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Error", status: "failed" })
+    }
 })
 
-router.put("/:id", authenticate, async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "This is post user API" })
+router.get("/:id", sessionAuthentication, async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ message: "List ID is required", status: "failed" });
+        }
+
+        const foundList = await List.findOne({ where: { id, userId: req.user?.id } });
+        if (!foundList) {
+            return res.status(404).json({ message: "List not found", status: "failed" });
+        }
+
+        return res.status(200).json({ message: "List retrieved successfully", data: foundList, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Error", status: "failed" })
+    }
 })
 
-router.delete("/:id", authenticate, async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "This is delete user API" })
+router.put("/:id", sessionAuthentication, async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+        if (!id) {
+            return res.status(400).json({ message: "List ID is required", status: "failed" });
+        }
+        if (!title || !description) {
+            return res.status(400).json({ message: "Title and description is required", status: "failed" });
+        }
+        const foundList = await List.findOne({ where: { id, userId: req.user?.id } });
+        if (!foundList) {
+            return res.status(404).json({ message: "List not found", status: "failed" });
+        }
+        foundList.title = title
+        foundList.description = description
+        const savedList = await foundList.save();
+        return res.status(200).json({ message: "List updated successfully", data: savedList, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Error", status: "failed" })
+    }
+})
+
+router.delete("/:id", sessionAuthentication, async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ message: "List ID is required", status: "failed" });
+        }
+        const foundList = await List.findOne({ where: { id, userId: req.user?.id } });
+        if (!foundList) {
+            return res.status(404).json({ message: "List not found", status: "failed" });
+        }
+        await foundList.destroy();
+        return res.status(200).json({ message: "List deleted successfully", status: "success" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Error", status: "failed" })
+    }
 })
 
 export default router
